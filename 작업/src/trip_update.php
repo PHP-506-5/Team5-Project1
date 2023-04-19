35110655aa940e6fb3903e344068e6b91199b35a
xellos88 <parkjy0419@naver.com>
2023-04-19 17:35:44 +0900

    0419업데이트

--- 작업/src/trip_update.php
+++ 작업/src/trip_update.php
@@ -1,32 +1,36 @@
 <?php
-define("SRC_ROOT", $_SERVER["DOCUMENT_ROOT"] . "/");
-define("URL_DB", SRC_ROOT . "작업/common/trip_DB_conn.ybk.php");
-include_once(URL_DB);
+	define( "SRC_ROOT", $_SERVER["DOCUMENT_ROOT"]."/" );
+	// define( "URL_DB", SRC_ROOT."작업/common/trip_DB_conn_jyp.php" );
+    define( "URL_DB", SRC_ROOT."작업/common/trip_DB_conn.ybk.php" );
+	include_once( URL_DB );
 
-$http_method = $_SERVER["REQUEST_METHOD"];
+    $http_method = $_SERVER["REQUEST_METHOD"];
 
-// GET 일 때
-if ($http_method === "GET") {
+// GET 일때
+if( $http_method === "GET" )
+{
     $trip_no = 1;
-    if (array_key_exists("trip_no", $_GET)) {
+    if( array_key_exists( "trip_no", $_GET ) )
+    {
         $trip_no = $_GET["trip_no"];
     }
-    $result_info = select_trip_info_no($trip_no);
-} else {
-    $arr_post = $_POST;
+    $result_info = select_trip_info_no( $trip_no );
+}
+else{
+    $arr_post=$_POST;
     $arr_info = array(
-        "trip_no" => $arr_post["trip_no"],
-        "trip_title" => $arr_post["trip_title"],
-        "trip_city" => $arr_post["trip_city"],
-        "trip_date" => $arr_post["trip_date"],
-        "trip_price" => $arr_post["trip_price"],
-        "trip_contents" => $arr_post["trip_contents"]
-    );
-
+            "trip_no" => $arr_post["trip_no"]
+            ,"trip_title" => $arr_post["trip_title"]
+            ,"trip_city" => $arr_post["trip_city"]
+            ,"trip_date" => $arr_post["trip_date"]
+            ,"trip_price" => $arr_post["trip_price"]
+            ,"trip_contents" => $arr_post["trip_contents"]
+        );
+    
     // update
-    $result_cnt = update_trip_info_no($arr_info);
+    $result_cnt = update_trip_info_no( $arr_info );
 
-    header("Location: trip_detail.php?trip_no=" . $arr_post["trip_no"]);
+    header("Location: trip_detail.php?trip_no=".$arr_post["trip_no"] );
     exit();
 }
 ?>
@@ -57,52 +61,25 @@ if ($http_method === "GET") {
     <main class="page-main">
     <form method="post" action="trip_update.php">
         <div class="container">
-			<table class='aaa'>
-				<tr>
-					<th class="">
-						<label for="title">제목</label>
-					</th>
-					<td class="">
-						<input type="text" name="trip_title" id="title" value="<?php echo $result_info["trip_title"] ?>">
-					</td>
-				</tr>
-                <tr>
-					<th class="">
-						<label for="city">도시</label>
-					</th>
-					<td class="">
-						<input type="text" name="trip_city" id="city" value="<?php echo $result_info["trip_city"] ?>">
-					</td>
-				</tr>
-                <tr>
-					<th class="">
-						<label for="date">날짜</label>
-					</th>
-					<td class="">
-						<input type="date" name="trip_date" id="date" value="<?php echo $result_info["trip_date"] ?>">
-					</td>
-				</tr>
-                <tr>
-					<th class="">
-						<label for="pirce">비용</label>
-					</th>
-					<td class="">
-						<input type="pirce" name="trip_price" id="pirce" value="<?php echo $result_info["trip_price"] ?>">
-					</td>
-				</tr>
-				<tr>
-					<th class="">
-						<label  for="trip_contents">내용</label>
-					</th>
-					<td class="">
-					<textarea rows="6" cols="10" name="trip_contents" id="contents"><?php echo $result_info["trip_contents"] ?></textarea>
-					</td>
-				</tr>
-			</table>
+            <label for="title"> 제목 </label>
+            <input type="text" name="trip_title" id="title" required value="<?php echo $result_info["trip_title"] ?>">   
+            <br>
+            <label for="city"> 도시 </label>
+            <input type="text" name="trip_city" id="city" required autofocus value="<?php echo $result_info["trip_city"] ?>">
+            <br>
+            <label for="data"> 날짜 </label>
+            <input type="datetime-local" name="trip_date" id="date" value="<?php echo $result_info["trip_date"] ?>">
+            <br>
+            <label for="price"> 비용 </label>
+            <input type="text" name="trip_price" id="price" value="<?php echo $result_info["trip_price"] ?>">
+            <br>
+            <label for="contents"> 내용 </label>
+            <textarea rows="6" cols="40" name="trip_contents" id="contents"><?php echo $result_info["trip_contents"] ?></textarea>
+            <input type="hidden" name="trip_no" value="<?php echo $result_info["trip_no"] ?>">
         </div>
         <div class="button">
             <button type="submit" class="btn">수정</button>
-            <a href="trip_detail.php?trip_no=<?php echo $result_info["trip_no"] ?>" class="">취소</a>
+            <button type="button"><a href="trip_detail.php?trip_no=<?php echo $result_info["trip_no"] ?>" class="">취소</a></button>
 		</div>
     </form>
     </main>

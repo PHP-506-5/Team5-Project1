449900ddfaf06c5f654197e8ab4ed8e24e15c915
kimyeongbeom <faer9876@naver.com>
2023-04-24 11:05:38 +0900

    0424 폰트수정

--- workspace/src/trip_list.php
+++ workspace/src/trip_list.php
@@ -61,6 +61,8 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 	<title>여행 정보</title>
 	<link rel="stylesheet" href="../css/list.css">
+	<link rel="stylesheet" href="../css/font.css">
+	<link href="https://fonts.googleapis.com/css2?family=Gothic+A1:wght@500&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="../css/com_footer.css">
 	
 </head>

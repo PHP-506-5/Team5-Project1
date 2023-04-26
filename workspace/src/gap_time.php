7a04aeef5219df34b1056fcd41b91a940890d836
kimyeongbeom <faer9876@naver.com>
2023-04-26 10:09:03 +0900

    .

--- workspace/src/gap_time.php
+++ workspace/src/gap_time.php
@@ -3,15 +3,13 @@
 	
         $start_time = strtotime($start_date);
         $end_time = strtotime($end_date);
-    
+
         $diff = $end_time - $start_time;
-    
+
         $hours = floor($diff/3600);
-    
         $min = floor($diff/60);
     
         return sprintf("%d%d", $hours, $min); 
-    
     }
     // $a=print gap_time("2023-04-19 12:42","2023-04-19 13:00");"\n";
     // echo "\n";
